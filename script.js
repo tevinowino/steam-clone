f61301ce4a6c4a73a02df263ad7f600841ca3cb6
Tevin Owino <tevinowino65@gmail.com>
2026-03-17 10:33:20 +0300

    Added more games

--- script.js
+++ script.js
@@ -36,7 +36,39 @@ let games = [
         image: "images/space.png",
         tags: ["Survival", "Space"],
         price: "$29.99"
+    }, 
+    {
+        name: "Minecraft",
+        image: "images/minecraft.png",
+        tags: ["Open-World", "RPG"],
+        price: "$10.99"
+    },
+    {
+        name: "EFootball 2026",
+        image: "images/efootball.png",
+        tags: ["Sports", "Simulation"],
+        price: "$24.99"
+    },
+    {
+        name: "Black Desert",
+        image: "images/black-desert.png",
+        tags: ["MMORPG", "Adventure"],
+        price: "$24.99"
+    },
+    {
+        name: "GTA V",
+        image: "images/gta-v.png",
+        tags: ["Action", "RPG"],
+        price: "$24.99"
+    },
+    {
+        name: 'Uncharted 4: A Thief\'s End',
+        image: 'images/uncharted.png',
+        tags: ['Action', 'Adventure'],
+        price: '$24.99'
     }
+
+
 ]
 
 function displayGames() {
@@ -59,7 +91,6 @@ function displayGames() {
             </div>
             </div>
         `
-
     })
 }
 
